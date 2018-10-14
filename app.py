c1243d2d0bf61b723862d1a9a1a9eb2276273cf0
Yusuke Otsuki <whitehawk175s70c@gmail.com>
2018-10-14 21:41:33 +0900

    initial commit

--- app.py
+++ app.py
@@ -6,23 +6,12 @@ app = Flask(__name__)
 app.debug = True
 
 
-@app.route("/")
-def index():
-    return u"test"
 
-@app.route("/hello/<name>")
-def hello(name=""):
-    if name == "":
-        name = u"anonimous"
-    return render_template("hello.html",name=name)
-
-@app.route("/debug")
-def debug():
-    return render_template("notemplate.html")
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
-    app.run(port=port)
-
+@app.route('/echo', methods=['POST'])
+def echo():
+    message = request.json.get("result").get("parameters").get("message")
+    response = {
+        "speech": message + message
+    }
+    return jsonify(response)
 
