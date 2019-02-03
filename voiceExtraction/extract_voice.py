b4f544cf13cb444e8a579816df0b9ad8c095a11d
Yusuke Otsuki <whitehawk175s70c@gmail.com>
2019-02-03 22:52:47 +0900

    Make it easy for apply to much dataset

--- voiceExtraction/extract_voice.py
+++ voiceExtraction/extract_voice.py
@@ -4,31 +4,33 @@ from selenium.webdriver.common.action_chains import ActionChains
 import urllib.request
 import time
 
-filepath="data/test/1.wav"
-outfilename = "out.wav"
+filepath    = "data/input/******" + ".wav"
+outfilename = "data/output/******" + ".wav"
+driver_path = "/Users/yusukeotsuki/Downloads/chromedriver"
 
-
-driver= webdriver.Chrome(executable_path="/Users/yusukeotsuki/Downloads/chromedriver")
-actions = ActionChains(driver)
+driver= webdriver.Chrome(executable_path=driver_path)
 driver.get("http://unetvocalsep.herokuapp.com/")
 time.sleep(5)
 
+def extract_voice(infile_path, outfile_path)
+    
+    JS_DROP_FILE = "var tgt=arguments[0],e=document.createElement('input');e.type='"+\
+    "file';e.addEventListener('change',function(event){var dataTrans" +\
+    "fer={dropEffect:'',effectAllowed:'all',files:e.files,items:{},t" +\
+    "ypes:[],setData:function(format,data){},getData:function(format" +\
+    "){}};var emit=function(event,target){var evt=document.createEve" +\
+    "nt('Event');evt.initEvent(event,true,false);evt.dataTransfer=da"+\
+    "taTransfer;target.dispatchEvent(evt);};emit('dragenter',tgt);em"+\
+    "it('dragover',tgt);emit('drop',tgt);document.body.removeChild(e"+\
+    ");},false);document.body.appendChild(e);return e;"
 
-JS_DROP_FILE = "var tgt=arguments[0],e=document.createElement('input');e.type='"+\
-"file';e.addEventListener('change',function(event){var dataTrans" +\
-"fer={dropEffect:'',effectAllowed:'all',files:e.files,items:{},t" +\
-"ypes:[],setData:function(format,data){},getData:function(format" +\
-"){}};var emit=function(event,target){var evt=document.createEve" +\
-"nt('Event');evt.initEvent(event,true,false);evt.dataTransfer=da"+\
-"taTransfer;target.dispatchEvent(evt);};emit('dragenter',tgt);em"+\
-"it('dragover',tgt);emit('drop',tgt);document.body.removeChild(e"+\
-");},false);document.body.appendChild(e);return e;"
+    drop_area = driver.find_element_by_id("dropbox")
 
-drop_area = driver.find_element_by_id("dropbox")
+    tmp = driver.execute_script(JS_DROP_FILE, drop_area)
+    tmp.send_keys(infile_path)
 
-tmp = driver.execute_script(JS_DROP_FILE, drop_area)
-tmp.send_keys(filepath)
+    url = driver.find_element_by_id("audio-vocal").get_attribute("src")
+    urllib.request.urlretrieve(url, outfilename)
 
-url = driver.find_element_by_id("audio-vocal").get_attribute("src")
-urllib.request.urlretrieve(url, outfilename)
+extract_voice(filepath, outfilename)
 
